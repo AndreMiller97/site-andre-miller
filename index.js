a21ee86cca809f84bc0fd7ca984cab9424f7afae
AndreMiller97 <andre.miller97@hotmail.com>
2023-01-05 19:42:55 -0300

    add JS to project and create events

---
+++ index.js
@@ -0,0 +1,4 @@
+/* Aproveitando a mesma função de click passando via parametro o id, para evitar repetição de codigos*/
+function onClickScrollTo(id){
+    document.getElementById(id).scrollIntoView();
+}
